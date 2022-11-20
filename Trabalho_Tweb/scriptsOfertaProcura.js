852c5db938561912514f9ecaa4d7763603381224
Berna.rv.dev <bernardo.rv.dev@gmail.com>
2022-11-20 14:11:03 +0000

    script que não estava em uso

--- Trabalho_Tweb/scriptsOfertaProcura.js
+++
@@ -1,9 +0,0 @@
-function opcaoCasa() {
-    value = document.getElementById("tipoAlojamento").value;
-    if (value == "2") {
-
-        document.getElementById("opcaoDeCasa").style.display = "";
-    } else {
-        document.getElementById("opcaoDeCasa").style.display = "none";
-    }
-}
